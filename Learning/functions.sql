6e74de1dfc51e8fa0611f8600bde3d5386240e99
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2023-06-27 23:44:40 +0530

    added files containing new concepts

---
+++ Learning/functions.sql
@@ -0,0 +1,7 @@
+select round(avg(amount),2) from payment
+
+-- select * from payment
+-- copy payment(customer_id,	amount,	payment_mode,	payment_date)
+-- FROM 'C:\Program Files\PostgreSQL\15\data\payment.csv'
+-- DELIMITER ','
+-- CSV HEADER;
---
+++ Learning/test-1.sql
@@ -0,0 +1,19 @@
+SELECT * FROM customer
+ORDER BY custname DESC
+
+-- DELETE FROM customer
+-- WHERE custid=3;
+
+-- UPDATE 	customer 
+-- SET custname = 'Creed', age = 93
+-- WHERE custid=1;
+
+-- INSERT INTO customer 
+-- (CustID, CustName, Age, City, Salary)
+-- VALUES 
+-- (1, 'Sam', 26, 'Delhi', 3000),
+-- (2, 'Pam', 29, 'Scranton', 2345),
+-- (3, 'Jim', 74, 'Stramford', 3240),
+-- (4, 'Ryan', 30, 'Conneticut', 1000);
+
+-- SELECT * FROM customer
