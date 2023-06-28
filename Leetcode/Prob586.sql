3ea613655fb528183d23651de784547aa5d2e18a
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2023-06-28 17:03:19 +0530

    to find max number of something- find count order by it and limit to 1

--- Leetcode/Prob586.sql
+++ Leetcode/Prob586.sql
@@ -1,13 +1,6 @@
 # Write your MySQL query statement below
-SELECT
-    customer_number
-FROM
-    Orders
-GROUP BY
-    customer_number
-HAVING
-    COUNT(customer_number) > 1
-ORDER BY
-    COUNT(customer_number) DESC
-LIMIT
-    1
+SELECT customer_number
+FROM Orders
+GROUP BY customer_number
+ORDER BY COUNT(customer_number) DESC
+LIMIT 1
