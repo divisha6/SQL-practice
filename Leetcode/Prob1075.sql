ed820d7d1707ef1a672405547b8ac057f39be7e2
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2023-06-28 17:52:06 +0530

    INNER JOIN PS

---
+++ Leetcode/Prob1075.sql
@@ -0,0 +1,6 @@
+# Write your MySQL query statement below
+SELECT Project.project_id, ROUND(AVG(Employee.experience_years),2) AS average_years
+FROM Project 
+INNER JOIN Employee
+ON Employee.employee_id = Project.employee_id
+GROUP BY Project.project_id
