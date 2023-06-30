f08acd984ab392127ec48c37a7f4036312a1b8a1
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2023-06-30 20:27:37 +0530

    comparing the starting letter of a string attribute

---
+++ Hackerrank/Weather-6.sql
@@ -0,0 +1,13 @@
+/*
+ Enter your query here.
+ */
+SELECT
+    DISTINCT CITY
+FROM
+    STATION
+WHERE
+    UPPER(LEFT(CITY, 1)) = 'A'
+    OR UPPER(LEFT(CITY, 1)) = 'E'
+    OR UPPER(LEFT(CITY, 1)) = 'I'
+    OR UPPER(LEFT(CITY, 1)) = 'O'
+    OR UPPER(LEFT(CITY, 1)) = 'U'
