67a6589a3fc4e0dbca6b55ff52d52e205158230d
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2023-07-02 16:31:29 +0530

    triangle judgement

---
+++ Leetcode/Prob610.sql
@@ -0,0 +1,11 @@
+-- # Write your MySQL query statement below
+SELECT
+    *,
+    CASE
+        WHEN x + y <= z
+        OR y + z <= x
+        OR x + z <= y THEN "No"
+        ELSE "Yes"
+    END AS triangle
+FROM
+    Triangle
