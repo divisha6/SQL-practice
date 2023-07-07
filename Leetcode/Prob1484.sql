0252b3d18b0920acbc58a735c644baa9a7a7294c
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2023-07-08 00:33:01 +0530

    GROUP CONCAT for concatenating strings

---
+++ Leetcode/Prob1484.sql
@@ -0,0 +1,9 @@
+-- # Write your MySQL query statement below
+SELECT
+    DISTINCT sell_date,
+    COUNT(DISTINCT product) AS num_sold,
+    GROUP_CONCAT(DISTINCT product) AS products
+FROM
+    Activities
+GROUP BY
+    sell_date;
