a9dc852520a87dab4ae32cb51f43f488713fdd7a
2020.divisha.gupta@ves.ac.in <2020.divisha.gupta@ves.ac.in>
2023-07-11 17:10:38 +0530

    IMP - AVG with if condition

---
+++ Leetcode/Prob1934.sql
@@ -0,0 +1,6 @@
+-- # Write your MySQL query statement below
+SELECT u.user_id, ROUND(AVG(if(c.action="confirmed",1,0)) ,2) as confirmation_rate
+FROM Signups u
+LEFT JOIN Confirmations c
+ON u.user_id = c.user_id
+GROUP BY user_id
